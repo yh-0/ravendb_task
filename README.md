d44858dd29990a318e97057f83ac5036248826f7
Kuba Kucharski <kuba.kucharski15@gmail.com>
2024-11-22 01:27:32 +0100

    add python script

--- README.md
+++ README.md
@@ -7,21 +7,28 @@
 
 ### Usage
 
-Clone the repository
+Clone the repository and build the C# project
 ```bash
 git clone https://github.com/yh-0/ravendb_task.git
 cd ravendb_task
+dotnet build GetTvShowTotalLength.csproj
+```
+
+Set the C# application binary path as environment variable
+```bash
+# Linux
+export GET_TVSHOW_TOTAL_LENGTH_BIN="$(pwd)/bin/Debug/net9.0/GetTvShowTotalLength"
+# Windows
+set GET_TVSHOW_TOTAL_LENGTH_BIN=%cd%\bin\Debug\net9.0\GetTvShowTotalLength.exe
 ```
 
 To run Python script
 ```bash
-python3 tv-time.py tv-shows.txt
+python3 tv-time.py <show_list_file>
 ```
 
 To run C# application
 ```bash
-dotnet build GetTvShowTotalLength.csproj
 cd ./bin/Debug/net9.0
-chmod +x GetTvShowTotalLength
-./GetTvShowTotalLength
+./GetTvShowTotalLength <show_name> # wrap show name in double quotes if it contains space
 ```
---
+++ tv-time.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import asyncio
+from pathlib import Path
+from argparse import ArgumentParser
+
+
+CS_APP_PATH: Path = Path(os.getenv("GET_TVSHOW_TOTAL_LENGTH_BIN"))
+
+
+async def get_show_length(show: str, lengths_by_show: dict[str, int]) -> int:
+    process = await asyncio.create_subprocess_exec(
+        CS_APP_PATH,
+        f'"{show}"',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    stdout, _ = await process.communicate()
+    if process.returncode == 0:
+        lengths_by_show[show] = int(stdout.decode().strip())
+    else:
+        print(f"Could not get info for '{show}'.", file=sys.stderr)
+
+
+def format_item(item: tuple[str, int]) -> str:
+    show = item[0]
+    hours = int(item[1] / 60)
+    minutes = int(item[1] % 60)
+    return f"{show} ({hours}h {minutes}m)"
+
+
+def find_shortest_and_longest(lengths_by_show: dict[str, int]) -> None:
+    sorted_dict = dict(sorted(lengths_by_show.items(), key=lambda item: item[1]))
+    shortest = next(iter(sorted_dict.items()))
+    longest = next(iter(reversed(sorted_dict.items())))
+    print(f"The shortest show: {format_item(shortest)}")
+    print(f"The longest show: {format_item(longest)}")
+
+
+async def main() -> None:
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("SHOW_LIST_FILE")
+    args = arg_parser.parse_args()
+    show_list_file = args.SHOW_LIST_FILE
+    show_list_file_path = Path(show_list_file)
+
+    if CS_APP_PATH is None:
+        print("Environment variable GET_TVSHOW_TOTAL_LENGTH_BIN doesn't exist. Exiting.")
+        exit(0)
+
+    if not os.path.exists(CS_APP_PATH):
+        print(f"Path {CS_APP_PATH} doesn't exist. Make sure to build C# project with")
+        print(f"dotnet build GetTvShowTotalLength.csproj")
+        exit(0)
+
+    with open(show_list_file_path, "r") as file:
+        show_list = file.read().splitlines()
+
+    tasks = []
+    lengths_by_show: dict[str, int] = {}
+    for show in show_list:
+        tasks.append(get_show_length(show, lengths_by_show))
+    await asyncio.gather(*tasks)
+
+    find_shortest_and_longest(lengths_by_show)
+
+if __name__ == "__main__":
+    asyncio.run(main())
